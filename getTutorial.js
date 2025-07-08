53b103cfe1a6b7f69aa5058ba6b9c6c57de7c16b
VanesStalkers <ivan.mironov@bk.ru>
2025-07-09 02:39:36 +0400

    fixed getTutorial source priority

--- getTutorial.js
+++ getTutorial.js
@@ -1,11 +1,8 @@
 (formattedPath) => {
   const path = formattedPath.split('-');
-  let realPath = ['lib', ...path];
-  let obj = lib.utils.getDeep(this, realPath);
-  if (!obj) {
-    realPath = ['domain', ...path];
-    obj = lib.utils.getDeep(this, realPath);
-  }
+
+  const obj = lib.utils.getDeep(this, ['domain', ...path]) || lib.utils.getDeep(this, ['lib', ...path]);
+  
   if (!obj?.steps) throw new Error(`Tutorial "${formattedPath}" not found`);
   return obj;
 };
